47a21f0a4086d73e4e0df6ed370a8240557db978
Kensousen <dafede333@gmail.com>
2016-08-31 00:22:22 +0200

    First Commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	public GameObject dirLi;
+	bool activeLi = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("z")) {
+			if (activeLi) {
+				dirLi.SetActive (false);
+				activeLi = false;
+			} else {
+				dirLi.SetActive (true);
+				activeLi = true;
+			}
+
+		}
+	}
+}
